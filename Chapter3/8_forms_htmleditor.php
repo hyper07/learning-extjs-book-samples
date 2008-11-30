7e2a54ad4c50574cf5ea2dae09ce74a9a6db3b68
justarock <justarock@8d6c58a8-be5d-11dd-871e-457e4afae058>
2008-11-30 15:01:46 +0000

    Fixed typos in code samples

--- Chapter3/8_forms_htmleditor.php
+++ Chapter3/8_forms_htmleditor.php
@@ -8,12 +8,14 @@
 	Ext.onReady(function(){
 		Ext.BLANK_IMAGE_URL = 'images/s.gif';
 		Ext.QuickTips.init();
-		Ext.form.VTypes["nameVal"]  = /^([A-Z]{1})[A-Za-z\-]+ ([A-Z]{1})[A-Za-z\-]+/;
-		Ext.form.VTypes["nameMask"] = /[A-Za-z\- ]/;
-		Ext.form.VTypes["nameText"] = 'In-valid Director Name.';
-		Ext.form.VTypes["name"] 	= function(v){
-			return Ext.form.VTypes["nameVal"].test(v);
-		}
+        
+        Ext.form.VTypes.nameVal  = /^([A-Z]{1})[A-Za-z\-]+ ([A-Z]{1})[A-Za-z\-]+/;
+		Ext.form.VTypes.nameMask = /[A-Za-z\- ]/;
+		Ext.form.VTypes.nameText = 'In-valid Director Name.';
+		Ext.form.VTypes.name 	= function(v){
+			return Ext.form.VTypes.nameVal.test(v);
+		};
+        
 	    var genres = new Ext.data.SimpleStore({
 	        fields: ['id', 'genre'],
 	        data : [['1','Comedy'],['2','Drama'],['3','Action']]
